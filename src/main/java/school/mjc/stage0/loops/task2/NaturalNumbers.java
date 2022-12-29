9a1cb7877282b3e48058a24287a99721be2d0100
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-29 17:38:38 +0400

    Updated

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,9 +2,5 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
-        while (lastPrinted != 0){
-            System.out.println(0);
-            lastPrinted--;
-        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,10 +2,6 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        int i = 0;
-        while (i < chars.length) {
-            System.out.print(chars[i]);
-            i++;
-        }
+
     }
 }
