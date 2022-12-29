127bb9f69256ee5a76c3ac095293dd85cc8c7a75
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-29 17:22:30 +0400

    Updated file

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -3,10 +3,10 @@ package school.mjc.stage0.loops.task2;
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
         int number = 0;
-        while (printTillInclusive >= 0){
+        while (printTillInclusive >= 0) {
             System.out.println(number);
-            number+=2;
-            printTillInclusive-=2;
+            number += 2;
+            printTillInclusive -= 2;
         }
     }
 }
